2e4f94bda6daf2e7ae3edf1fd3ffe5d860a74c89
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-12-04 22:42:02 +0700

    RUN Instruction

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 # Perintah untuk build dari docker file dari folder from
+# FROM Instruction
 docker build -t ervlsa/from from
+
+# RUN Instruction
+docker build -t ervlsa/run run
+
+docker build -t ervlsa/run run --progress=plain --no-cache
 
 # Perintah untuk melihat seluruh docker image di komputer
 docker image ls
+
+docker image ls | grep ervlsa
