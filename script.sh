ba515b53f3e86e4a64faf701d5032f7dcc57771f
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-12-04 22:53:13 +0700

    COMMAND Instruction

--- script.sh
+++ script.sh
@@ -7,6 +7,17 @@ docker build -t ervlsa/run run
 
 docker build -t ervlsa/run run --progress=plain --no-cache
 
+# CMD Instruction
+docker build -t ervlsa/command command
+
+docker image inspect ervlsa/command
+
+docker container create --name command ervlsa/command
+
+docker container start command
+
+docker container logs command
+
 # Perintah untuk melihat seluruh docker image di komputer
 docker image ls
 
